3ad15ed0164fdfdd81df5e9c7fc39b21614491e8
Skyler Parr <skyler.parr@breakoutbit.com>
2016-02-25 08:17:33 -0800

    we've added a grumpy cat =)

--- Sources/LiveGame.hx
+++ Sources/LiveGame.hx
@@ -1,4 +1,9 @@
 package;
+import kha.System;
+import kha.ScreenRotation;
+import kha.Canvas;
+import kha.Scaler;
+import kha.Image;
 import kha.Color;
 import kha.Font;
 import kha.Assets;
@@ -6,27 +11,69 @@ import kha.Framebuffer;
 
 class LiveGame {
 
+    private var initialized: Bool = false;
     private var font: Font;
+    private var grumpyCat: Image;
+    private var xPos: Float = 0;
+
+    private var backbuffer: Image;
 
     public function new() {
+        backbuffer = Image.createRenderTarget(800, 600);
+
         Assets.loadFont("helveticaneue_light", onFontLoad);
+        Assets.loadImage("grumpy_cat_nope", onImageLoaded);
     }
 
     private function onFontLoad(font:Font):Void {
         this.font = font;
+        checkLoaded();
+    }
+
+    private function onImageLoaded(image:Image):Void {
+        grumpyCat = image;
+        checkLoaded();
+    }
+
+    private inline function checkLoaded():Void {
+        if(font != null && grumpyCat != null) {
+            initialized = true;
+        }
     }
 
     public function render(framebuffer:Framebuffer):Void {
+        if(!initialized) {
+            return;
+        }
+
+        var g = backbuffer.g2;
+        g.begin(false);
+        g.drawImage(grumpyCat, 0, 0);
+        g.end();
+
         var g = framebuffer.g2;
 
         g.begin();
+
+        //things will be rendered on bottom
+        Scaler.scale(backbuffer, framebuffer, System.screenRotation);
+
         g.font = font;
         g.fontSize = 32;
         g.color = Color.Pink;
-        g.drawString("Hello world", 0, 0);
+        g.drawString("Hello world", xPos, 0);
+
+        //things will be rendered on top
+
         g.end();
+
+        xPos++;
+        if(xPos > 800) {
+            xPos = 0;
+        }
     }
 
     public function update():Void {
+
     }
 }
---
+++ run.sh
@@ -0,0 +1,2 @@
+haxelib run kha osx --compile &&
+./build/osx-build/build/Release/Empty.app/Contents/MacOS/Empty; exit
