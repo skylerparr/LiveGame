1439bcebb34d06c6ba03883b43869262e2a3d931
Skyler Parr <skyler.parr@breakoutbit.com>
2016-02-26 08:19:39 -0800

    added an animated sprite wizard

--- Sources/LiveGame.hx
+++ Sources/LiveGame.hx
@@ -1,42 +1,43 @@
 package;
+import kha.Scheduler;
+import kha.Assets;
 import kha.System;
 import kha.ScreenRotation;
-import kha.Canvas;
 import kha.Scaler;
 import kha.Image;
 import kha.Color;
-import kha.Font;
-import kha.Assets;
 import kha.Framebuffer;
 
 class LiveGame {
 
-    private var initialized: Bool = false;
-    private var font: Font;
-    private var grumpyCat: Image;
-    private var xPos: Float = 0;
+    private static inline var FRAME_SIZE: Float = 64;
+    private static inline var FRAME_COUNT: Float = 3;
+    private static inline var FRAME_TIME: UInt = 60;
 
+    private var initialized: Bool = false;
+    private var xPos: Float = 0;
+    private var yPos: Float = 0;
+
+    private var wizard: Image;
     private var backbuffer: Image;
+
+    private var currentTime: Float = 0;
 
     public function new() {
         backbuffer = Image.createRenderTarget(800, 600);
 
-        Assets.loadFont("helveticaneue_light", onFontLoad);
-        Assets.loadImage("grumpy_cat_nope", onImageLoaded);
+        currentTime = getNow();
+
+        Assets.loadImage("wizard", onWizardLoaded);
     }
 
-    private function onFontLoad(font:Font):Void {
-        this.font = font;
-        checkLoaded();
-    }
-
-    private function onImageLoaded(image:Image):Void {
-        grumpyCat = image;
+    private function onWizardLoaded(image: Image): Void {
+        wizard = image;
         checkLoaded();
     }
 
     private inline function checkLoaded():Void {
-        if(font != null && grumpyCat != null) {
+        if(wizard != null) {
             initialized = true;
         }
     }
@@ -47,30 +48,37 @@ class LiveGame {
         }
 
         var g = backbuffer.g2;
-        g.begin(false);
-        g.drawImage(grumpyCat, 0, 0);
+        g.begin();
+        g.drawSubImage(wizard, 0, 0, xPos, yPos, FRAME_SIZE, FRAME_SIZE);
         g.end();
 
         var g = framebuffer.g2;
 
         g.begin();
 
-        //things will be rendered on bottom
         Scaler.scale(backbuffer, framebuffer, System.screenRotation);
-
-        g.font = font;
-        g.fontSize = 32;
-        g.color = Color.Pink;
-        g.drawString("Hello world", xPos, 0);
-
-        //things will be rendered on top
 
         g.end();
 
-        xPos++;
-        if(xPos > 800) {
-            xPos = 0;
+        var now: Float = getNow();
+        if(now < currentTime + FRAME_TIME) {
+            return;
         }
+        currentTime = getNow();
+
+        xPos += FRAME_SIZE;
+        if(xPos > FRAME_COUNT * FRAME_SIZE) {
+            xPos = 0;
+            yPos += FRAME_SIZE;
+
+            if(yPos > FRAME_COUNT * FRAME_SIZE) {
+                yPos = 0;
+            }
+        }
+    }
+
+    private inline function getNow(): Float {
+        return Scheduler.time() * 1000;
     }
 
     public function update():Void {
