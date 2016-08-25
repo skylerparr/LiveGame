4d83e146bcef4d59d512cfe64d81e7b3d9d4e380
Skyler Parr <skyler.parr@breakoutbit.com>
2016-08-25 08:22:00 -0700

    added game world interaction manager tests

--- Sources/input/BattleGameWorldInteractionManager.hx
+++ Sources/input/BattleGameWorldInteractionManager.hx
@@ -12,8 +12,12 @@ class BattleGameWorldInteractionManager implements GameWorldInteractionManager {
     }
 
     function set_interactionContainer(value:DisplayNodeContainer) {
-        value.registerPointerEvent(PointerEventType.POINTER_1_CLICK, onGameWorldClicked);
-        return this.interactionContainer = value;
+        unRegisterAllEvents();
+        this.interactionContainer = value;
+        if(value != null) {
+            registerAllEvents();
+        }
+        return interactionContainer;
     }
 
     public function new() {
@@ -23,10 +27,88 @@ class BattleGameWorldInteractionManager implements GameWorldInteractionManager {
     }
 
     public function dispose():Void {
+        gameInputTools = null;
+        interactionContainer = null;
     }
 
-    private function onGameWorldClicked(e:PointerEvent):Void {
-        trace("clicked the game world");
-        trace('${e.screenX}, ${e.screenY}');
+    private inline function registerAllEvents(): Void {
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_1_CLICK, onGameWorldPointer1Click);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_1_UP, onGameWorldPointer1Up);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_1_DOWN, onGameWorldPointer1Down);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_2_CLICK, onGameWorldPointer2Click);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_2_UP, onGameWorldPointer2Up);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_2_DOWN, onGameWorldPointer2Down);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_3_CLICK, onGameWorldPointer3Click);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_3_UP, onGameWorldPointer3Up);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_3_DOWN, onGameWorldPointer3Down);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_1_DOUBLE_CLICK, onGameWorldPointer1DoubleClick);
+        interactionContainer.registerPointerEvent(PointerEventType.ZOOM, onGameWorldZoom);
+        interactionContainer.registerPointerEvent(PointerEventType.POINTER_MOVE, onGameWorldPointerMove);
     }
+
+    private inline function unRegisterAllEvents():Void {
+        if(interactionContainer != null) {
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_1_CLICK, onGameWorldPointer1Click);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_1_UP, onGameWorldPointer1Up);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_1_DOWN, onGameWorldPointer1Down);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_2_CLICK, onGameWorldPointer2Click);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_2_UP, onGameWorldPointer2Up);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_2_DOWN, onGameWorldPointer2Down);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_3_CLICK, onGameWorldPointer3Click);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_3_UP, onGameWorldPointer3Up);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_3_DOWN, onGameWorldPointer3Down);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_1_DOUBLE_CLICK, onGameWorldPointer1DoubleClick);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.ZOOM, onGameWorldZoom);
+            interactionContainer.unRegisterPointerEvent(PointerEventType.POINTER_MOVE, onGameWorldPointerMove);
+        }
+    }
+
+    private function onGameWorldPointer1Click(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerClick(e);
+    }
+
+    private function onGameWorldPointer1Up(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerUp(e);
+    }
+
+    private function onGameWorldPointer1Down(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerDown(e);
+    }
+
+    private function onGameWorldPointer2Click(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerRightClick(e);
+    }
+
+    private function onGameWorldPointer2Up(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerRightUp(e);
+    }
+
+    private function onGameWorldPointer2Down(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerRightDown(e);
+    }
+
+    private function onGameWorldPointer3Click(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerMiddleClick(e);
+    }
+
+    private function onGameWorldPointer3Up(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerMiddleUp(e);
+    }
+
+    private function onGameWorldPointer3Down(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerMiddleDown(e);
+    }
+
+    private function onGameWorldPointer1DoubleClick(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerDoubleClick(e);
+    }
+
+    private function onGameWorldZoom(e:PointerEvent):Void {
+        gameInputTools.currentTool.onScroll(e);
+    }
+
+    private function onGameWorldPointerMove(e:PointerEvent):Void {
+        gameInputTools.currentTool.onPointerMove(e);
+    }
+
 }
--- test/input/BattleGameWorldInteractionManagerTest.hx
+++ test/input/BattleGameWorldInteractionManagerTest.hx
@@ -1,5 +1,9 @@
 package input;
 
+import mocks.MockPointingTool;
+import mocks.MockDisplayNodeContainer;
+import mocks.MockGameInputTools;
+import display.DisplayNodeContainer;
 import massive.munit.util.Timer;
 import massive.munit.Assert;
 import massive.munit.async.AsyncFactory;
@@ -8,9 +12,24 @@ import mockatoo.Mockatoo.*;
 using mockatoo.Mockatoo;
 class BattleGameWorldInteractionManagerTest {
 
+    private var interactionManager: BattleGameWorldInteractionManager;
+    private var gameInputTools: MockGameInputTools;
+    private var container: MockDisplayNodeContainer;
+    private var currentTool: MockPointingTool;
 
     @Before
     public function setup():Void {
+        gameInputTools = mock(MockGameInputTools);
+        container = mock(MockDisplayNodeContainer);
+        currentTool = mock(MockPointingTool);
+
+        gameInputTools.get_currentTool().returns(currentTool);
+
+        interactionManager = new BattleGameWorldInteractionManager();
+        interactionManager.gameInputTools = gameInputTools;
+        interactionManager.init();
+
+        interactionManager.interactionContainer = container;
     }
 
     @After
@@ -18,7 +37,115 @@ class BattleGameWorldInteractionManagerTest {
     }
 
     @Test
-    public function testExample():Void {
-        Assert.isTrue(true);
+    public function shouldRegisterAllPointerEvents(): Void {
+        container.registerPointerEvent(PointerEventType.POINTER_1_CLICK, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_1_UP, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_1_DOWN, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_2_CLICK, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_2_UP, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_2_DOWN, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_3_CLICK, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_3_UP, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_3_DOWN, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_1_DOUBLE_CLICK, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.ZOOM, cast isNotNull).verify();
+        container.registerPointerEvent(PointerEventType.POINTER_MOVE, cast isNotNull).verify();
     }
+
+    @Test
+    public function shouldUnRegisterAllEventsOnPreviousContainerIfReassigned(): Void {
+        var newContainer: MockDisplayNodeContainer = mock(MockDisplayNodeContainer);
+        interactionManager.interactionContainer = newContainer;
+
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_UP, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_DOWN, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_UP, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_DOWN, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_UP, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_DOWN, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_DOUBLE_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.ZOOM, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_MOVE, cast isNotNull).verify();
+    }
+
+    @Test
+    public function shouldNotUnRegisterAllEventsIfHasNeverBeenAssigned(): Void {
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_CLICK, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_UP, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_DOWN, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_CLICK, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_UP, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_DOWN, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_CLICK, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_UP, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_DOWN, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_DOUBLE_CLICK, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.ZOOM, cast isNotNull).verify(0);
+        container.unRegisterPointerEvent(PointerEventType.POINTER_MOVE, cast isNotNull).verify(0);
+    }
+
+    @Test
+    public function shouldPassEventToCurrentGameTool(): Void {
+        var newContainer: MockDisplayNodeContainer = mock(MockDisplayNodeContainer);
+        var pe: PointerEvent = new PointerEvent();
+
+        simulateEvent(newContainer, PointerEventType.POINTER_1_CLICK, pe);
+        currentTool.onPointerClick(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_1_UP, pe);
+        currentTool.onPointerUp(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_1_DOWN, pe);
+        currentTool.onPointerDown(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_2_CLICK, pe);
+        currentTool.onPointerRightClick(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_2_UP, pe);
+        currentTool.onPointerRightUp(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_2_DOWN, pe);
+        currentTool.onPointerRightDown(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_3_CLICK, pe);
+        currentTool.onPointerMiddleClick(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_3_UP, pe);
+        currentTool.onPointerMiddleUp(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_3_DOWN, pe);
+        currentTool.onPointerMiddleDown(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_1_DOUBLE_CLICK, pe);
+        currentTool.onPointerDoubleClick(pe).verify();
+        simulateEvent(newContainer, PointerEventType.ZOOM, pe);
+        currentTool.onScroll(pe).verify();
+        simulateEvent(newContainer, PointerEventType.POINTER_MOVE, pe);
+        currentTool.onPointerMove(pe).verify();
+    }
+
+    @Test
+    public function shouldDispose(): Void {
+        interactionManager.dispose();
+
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_UP, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_DOWN, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_UP, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_2_DOWN, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_UP, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_3_DOWN, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_1_DOUBLE_CLICK, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.ZOOM, cast isNotNull).verify();
+        container.unRegisterPointerEvent(PointerEventType.POINTER_MOVE, cast isNotNull).verify();
+
+        Assert.isNull(interactionManager.gameInputTools);
+        Assert.isNull(interactionManager.interactionContainer);
+    }
+
+    private function simulateEvent(newContainer: MockDisplayNodeContainer, eventType: PointerEventType, pe: PointerEvent): Void {
+        var handler: PointerEvent->Void = null;
+        newContainer.registerPointerEvent(eventType, cast isNotNull).calls(function(args): Void {
+            handler = cast args[1];
+        });
+        interactionManager.interactionContainer = newContainer;
+        handler(pe);
+    }
+
 }
